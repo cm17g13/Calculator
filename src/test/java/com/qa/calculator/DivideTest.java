b7a3408a44109b47eaf153db91e5353620a925b0
cm17g13 <callummcgregor50@gmail.com>
2018-04-19 10:39:32 +0100

    Added divide tests

--- src/test/java/com/qa/calculator/DivideTest.java
+++ src/test/java/com/qa/calculator/DivideTest.java
@@ -3,14 +3,14 @@ package com.qa.calculator;
 import org.junit.Test;
 import org.junit.Assert;
 
-public class MultiplyTest {
+public class DivideTest {
 
-	private Multiply multiply;
+	private Divide Divide;
 	@Test
-	public void testMultiply() {
-		multiply = new Multiply();
+	public void testDivide() {
+		Divide = new Divide();
 		int expectedValue = 4;
-		int actualValue = multiply.multiply(2, 2);
+		int actualValue = Divide.divide(2, 2);
 		Assert.assertEquals(expectedValue, actualValue);
 	}
 }
