c32f12591ade178fe3d3d7aecd90c642bb69a76c
cm17g13 <callummcgregor50@gmail.com>
2018-04-19 10:35:31 +0100

    Added multiply class and method

--- src/test/java/com/qa/calculator/MultiplyTest.java
+++ src/test/java/com/qa/calculator/MultiplyTest.java
@@ -1,5 +1,16 @@
 package com.qa.calculator;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class MultiplyTest {
 
+	private Multiply multiply;
+	@Test
+	public void testAdd() {
+		multiply = new Multiply();
+		int expectedValue = 4;
+		int actualValue = multiply.multiply(2, 2);
+		Assert.assertEquals(expectedValue, actualValue);
+	}
 }
